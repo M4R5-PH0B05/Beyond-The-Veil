43b5129073ca5355c1521e65535268e3315386cb
rosetrussler <t017235p@student.staffs.ac.uk>
2026-02-01 15:40:31 +0000

    make

--- BeyondTheVeil/Assets/Scripts/DisappearingTile.cs
+++ BeyondTheVeil/Assets/Scripts/DisappearingTile.cs
@@ -44,12 +44,12 @@ public class DisappearingTile : MonoBehaviour
     /// <summary>
     /// The tile collider component
     /// </summary>
-    private BoxCollider2D m_collider;
+    private Collider2D m_collider;
 
     void Awake()
     {
         m_spriteRenderer = GetComponent<SpriteRenderer>();
-        m_collider = GetComponent<BoxCollider2D>();
+        m_collider = GetComponent<Collider2D>();
     }
 
     void Start()
